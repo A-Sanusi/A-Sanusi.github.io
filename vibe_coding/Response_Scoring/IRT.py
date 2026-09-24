0d24de886128d8ed46e4964035be2456f9e8a49b
A-Sanusi <143058699+A-Sanusi@users.noreply.github.com>
2026-09-24 14:10:31 +0700

    Update IRT.py

--- vibe_coding/Response_Scoring/IRT.py
+++ vibe_coding/Response_Scoring/IRT.py
@@ -34,7 +34,7 @@ excel_file = pd.ExcelFile(uploaded_file)
 selected_sheet = st.selectbox("Select Responses Sheet:", excel_file.sheet_names, key="sheet_resp")
 
 # Header=0 uses Excel Row 1 as Question ID headers
-df_resp_raw = pd.read_excel(uploaded_file, sheet_name=selected_sheet, header=0)
+df_resp_raw = pd.read_excel(uploaded_file, sheet_name=selected_sheet, header=1)
 
 # Slice from index 1 to start test takers from Excel Row 3 (skipping Excel Row 2)
 df_resp = df_resp_raw.iloc[1:].copy()
