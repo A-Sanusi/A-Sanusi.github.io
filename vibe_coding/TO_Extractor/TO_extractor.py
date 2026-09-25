11c818b32cc4c881ecdba4221621ffde9eccdc17
A-Sanusi <143058699+A-Sanusi@users.noreply.github.com>
2026-09-25 19:10:35 +0700

    Update TO_extractor.py

--- vibe_coding/TO_Extractor/TO_extractor.py
+++ vibe_coding/TO_Extractor/TO_extractor.py
@@ -100,6 +100,9 @@ for sheet in target_sheets:
 df_hasil = df_hasil.drop(columns=["key_match"]).dropna(subset=[nama_siswa_col])
 df_hasil = df_hasil.fillna("-")
 
+df_hasil = df_hasil.drop(columns=["key_match"]).dropna(subset=[Total_Benar_TO_col])
+df_hasil = round(df_hasil)
+
 # 3. Tampilkan Hasil & Fitur Export
 st.subheader("Tabel Nilai Siswa")
 
