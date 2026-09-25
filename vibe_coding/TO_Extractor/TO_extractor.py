06bd1ef4157809b7f7bf5dd428ac83ba525c84a2
A-Sanusi <143058699+A-Sanusi@users.noreply.github.com>
2026-09-25 19:39:38 +0700

    Update TO_extractor.py

--- vibe_coding/TO_Extractor/TO_extractor.py
+++ vibe_coding/TO_Extractor/TO_extractor.py
@@ -4,8 +4,8 @@ import pandas as pd
 import scipy.stats as stats
 import streamlit as st
 
-st.set_page_config(page_title="TRIM TO", layout="wide")
-st.title("TRIM Try Out")
+st.set_page_config(page_title="Ekstrak Nilai TO 🟣", layout="wide")
+st.title("Nilai TO kini ada Ekstraknya 🟣")
 
 col1, col2 = st.columns(2)
 
