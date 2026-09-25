fb4e55976a8cacb0050396474e594617e705217a
A-Sanusi <143058699+A-Sanusi@users.noreply.github.com>
2026-09-25 17:30:27 +0700

    Update TO_extractor.py

--- vibe_coding/TO_Extractor/TO_extractor.py
+++ vibe_coding/TO_Extractor/TO_extractor.py
@@ -93,8 +93,8 @@ student_data = df_hasil[df_hasil[nama_siswa_col] == selected_student].iloc[0]
 
 col_a, col_b, col_c = st.columns(3)
 with col_a:
-  st.metric(sheet_siswa_1, student_data.get("Nilai_sheet_siswa_1", "-"))
+  st.metric("1", student_data.get("Nilai_sheet_siswa_1", "-"))
 with col_b:
-  st.metric(sheet_siswa_2, student_data.get("Nilai_sheet_siswa_2", "-"))
+  st.metric("2", student_data.get("Nilai_sheet_siswa_2", "-"))
 with col_c:
-  st.metric(sheet_siswa_3, student_data.get("Nilai_sheet_siswa_3", "-"))
+  st.metric("3", student_data.get("Nilai_sheet_siswa_3", "-"))
