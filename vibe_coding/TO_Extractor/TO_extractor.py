46c768b545184ddac231505243b6daf3b74111e3
A-Sanusi <143058699+A-Sanusi@users.noreply.github.com>
2026-09-25 18:04:09 +0700

    Update TO_extractor.py

--- vibe_coding/TO_Extractor/TO_extractor.py
+++ vibe_coding/TO_Extractor/TO_extractor.py
@@ -79,7 +79,7 @@ for sheet in target_sheets:
         df_sub = df_sub.rename(columns={nilai_TO_col: f"Nilai_{sheet}"})
 
                 # Ambil kolom kunci dan nilai, lalu hapus duplikasi jika ada
-        df_sub_2 = df_nilai_raw_2[["key_match", kategori_TO_col]].drop_duplicates(
+        df_sub_2 = df_nilai_raw[["key_match", kategori_TO_col]].drop_duplicates(
             subset=["key_match"]
         )
         df_sub_2 = df_sub_2.rename(columns={kategori_TO_col: f"Kategori_{sheet}"})
