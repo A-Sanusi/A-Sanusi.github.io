fa014469b10dcd1f10555b298ff8f4114ae64fc7
A-Sanusi <143058699+A-Sanusi@users.noreply.github.com>
2026-09-26 19:19:25 +0700

    Update Data_extractor.py

--- vibe_coding/Data_Extractor/Data_extractor.py
+++ vibe_coding/Data_Extractor/Data_extractor.py
@@ -261,7 +261,7 @@ with tab_kehadiran:
             with cols[i]:
                 selecting[selected_bulan[i]] = st.selectbox(
                     f"Bulan {selected_bulan[i]}",
-                    uploaded_siswa_2.sheet_names,  # Uses pd.ExcelFile sheet names
+                    excel_siswa_2.sheet_names,  # Uses pd.ExcelFile sheet names
                     key=f"select_sheet_kehadiran_{i}",  # Unique key for loop
                 )
 
